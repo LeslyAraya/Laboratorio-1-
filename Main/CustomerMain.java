e12a896c1c230ae2a9929b3166f83493351451a6
jprodriguezb <jprodriguezb@gmail.com>
2024-09-11 11:15:38 -0600

    Modificacion CustomerMain
    Se modifica la clase CustomerMain aplicando MVC

--- Main/CustomerMain.java
+++ Main/CustomerMain.java
@@ -1,5 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package main;
 
+import Controllers.CustomerController;
+import Models.CustomerList;
 import Views.CustomerConsole;
 
 /**
@@ -12,7 +18,10 @@ public class CustomerMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {       
+        CustomerList model = new CustomerList();
         CustomerConsole view = new CustomerConsole();
+        CustomerController controller = new CustomerController(model,view);
+        view.setController(controller);
         view.show();
     }
     
