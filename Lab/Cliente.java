bb21e20ca27670730ebcef6c723905ba127398c3
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-09-18 14:36:38 -0600

    Se crea la clase Cliente
    Se crea la clase cliente con sus respectivos atributos

--- Lab/Cliente.java
+++ Lab/Cliente.java
@@ -4,10 +4,67 @@
  */
 package Lab;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author user
  */
 public class Cliente {
+    private String id;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String telefono;
+    private String correo;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Cliente(String id, String nombre, LocalDate fechaNacimiento, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public Cliente() {
+        this(null,null,LocalDate.now(),null, null);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", correo=" + correo + '}';
+    }
     
+       
 }
