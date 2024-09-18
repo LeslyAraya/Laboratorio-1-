8671680656c24f3f1f47bc360fedf7264949464b
Thyfa <thmartinezgo@est.utn.ac.cr>
2024-09-18 15:09:16 -0600

    Se realizan modificaciones

--- Lab/Expediente.java
+++ Lab/Expediente.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Lab;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author thyfa
  */
 public class Expediente {
-    
+    private List<Cita> citas;
+
+    public Expediente() {
+        this.citas = new ArrayList<>();
+    }
+
+    public void agregarCita(Cita cita) {
+        citas.add(cita);
+    }
+
+    public List<Cita> getCitas() {
+        return citas;
+    }
+
+    public void mostrarHistorial() {
+        for (Cita cita : citas) {
+            System.out.println("Cita: " + cita.getFechaHora());
+            cita.mostrarHojaMedica();
+            System.out.println("-------------------");
+        }
+    }
 }
+
--- Lab/HojaMedica.java
+++ Lab/HojaMedica.java
@@ -9,5 +9,43 @@ package Lab;
  * @author thyfa
  */
 public class HojaMedica {
+    private String signosVitales;
+    private String diagnostico;
+    private String observaciones;
+    private String medicamentos;
+    private String examenes;
+
+    public String getSignosVitales() {
+        return signosVitales;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public String getMedicamentos() {
+        return medicamentos;
+    }
+
+    public String getExamenes() {
+        return examenes;
+    }
+
+    public HojaMedica(String signosVitales, String diagnostico, String observaciones, String medicamentos, String examenes) {
+        this.signosVitales = signosVitales;
+        this.diagnostico = diagnostico;
+        this.observaciones = observaciones;
+        this.medicamentos = medicamentos;
+        this.examenes = examenes;
+    }
+
+    public HojaMedica() {
+        this("","","","","");
+    }
+    
     
 }
