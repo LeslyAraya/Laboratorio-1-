42bbcee41873fa57035b6565c6109fb3cce7de45
TessDuarte <tess.duarte07@gmail.com>
2024-09-18 14:16:04 -0600

    ClasePaciente

---
+++ Models/ClasePaciente.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.util.List;
+
+/**
+ *
+ * @author Tessd
+ */
+public class ClasePaciente {
+    private String nombre;
+    private String cedula;
+    private List<HistorialMedico> historialMedico;
+
+    public ClasePacienteng nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula
+}
+
+    private static class HistorialMedico {
+
+        public HistorialMedico() {
+        }
+    }
+
+    private static class ClasePacienteng {
+
+        public ClasePacienteng() {
+        }
+    }
